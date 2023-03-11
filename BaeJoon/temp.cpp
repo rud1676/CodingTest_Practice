627fc10252f98cfb094f4552f85590564ee26918
Joo <rud167637@gmail.com>
2023-03-11 17:30:06 +0900

    solve 틀린문제

--- BaeJoon/temp.cpp
+++ BaeJoon/temp.cpp
@@ -2,64 +2,40 @@
 
 using namespace std;
 
-int n,l,h;
-int mp[101][101];
-int vst[101][101];
-int dx[4]={0,0,1,-1};
-int dy[4]={1,-1,0,0};
+int vst[100001];
+int cnt[100001];
+int a,b;
 
-vector<pair<int,int>> v;
-int sum;
 
-bool check(int num){
-	if(num>=l && num<=h) return true;
-	return false;
-}
-void dfs(int y,int x){
-	v.push_back({y,x});
-	vst[y][x]=1;
-	sum+=mp[y][x];
-	for(int i=0;i<4;i++){
-		int ny = y+dy[i];
-		int nx = x+dx[i];
-		if(ny<0||nx<0||ny>=n||nx>=n||vst[ny][nx]!=0||check(abs(mp[ny][nx]-mp[y][x]))==false) continue;
-		dfs(ny,nx);
+void bfs(int a){
+
+	queue<int> q;
+	q.push(a);
+	while(q.size()){
+		int cur = q.front();q.pop();
+		for(int a:{cur-1,cur+1,cur*2}){
+			if(a<0||a>100000) continue;
+			if(vst[a]==0){
+				vst[a]=vst[cur]+1;
+				cnt[a] +=cnt[cur];
+				q.push(a);
+			}else if(vst[a]==vst[cur]+1) cnt[a]+=cnt[cur];
+		}
 	}
-
 }
 
 int main(){
-	cin >>n >>l >> h;
-
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			cin >> mp[i][j];
-		}
+	cin >> a>> b;
+	if(a==b){
+		cout << "0" << '\n'<<"1"<<'\n';
+		return 0;
 	}
 
-	int cnt=0;
-	int sig=true;
-	while(sig){
-		sig=false;
-		fill(&vst[0][0],&vst[0][0]+101*101,0);
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
-				if(vst[i][j]==0){
-					dfs(i,j);
-					if(sum>mp[i][j]){
-						sig=true;
-						int val = (int)(sum/v.size());
-						for(auto it:v){
-							mp[it.first][it.second]=val;
-						}
-					}
-				}
-				v.clear();
-				sum=0;
-			}
-		}
-		if(sig) cnt++;
-	}
-	cout << cnt <<'\n';
+	vst[a]=1;
+	cnt[a]=1;
+	bfs(a);
 
+
+	cout << vst[b]-1 << '\n' << cnt[b] << '\n';
+	return 0;
 }
